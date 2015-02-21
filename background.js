52906c7e5eee037443b5b0556915279a3d346689
alice <aliceworkmail@gmail.com>
2015-02-21 13:37:14 -0800

    adde dmy stuff

--- background.js
+++ background.js
@@ -1,49 +1,46 @@
-// React when a browser action's icon is clicked.
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
 
-$(document).ready(function() {
-  recognizer();
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
 });
 
-var recognizer = function() {
-  var recognition = new webkitSpeechRecognition();
-  recognition.interimResults = true;
-  //recognition.continuous = true;
-  recognition.onstart = function() {
-    console.log("listen");
-  };
+$(document).ready(function() {
+	console.log("BOO");
+	function addRow(command, url) {
+		$("#settingsTable").find('tbody')
+	    .append($('<tr>')
+	        .append($('<td>')
+	            .append('<input type="text" name="command" value={0} >'.format(command)))
+	        .append($('<td>')
+	            .append('<input type="text" name="url" value={0} >'.format(url)))
+	        .append($('<td>')
+	            .append('<button type="button" class="delete">Delete</button>'))
+	    )
+	}
+	
+	addRow("youtube", "www.youtube.com");
 
-  recognition.onerror = function(event) {
-    console.log("ERROR: " + event.error);
-  };
+	$('.delete').bind('click', function(e) {
+	    var row = e.target.parentNode.parentNode;
+	    $( row ).remove(); 
+	});
 
-  recognition.onend = function(event) {
-    console.log("finished: ");
-    console.log(event);
-    recognition.start();
-  };
-
-  recognition.onresult = function(event) {
-    var interim_transcript = '';
-    var final_transcript = '';
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
-    console.log("FINAL: " + final_transcript);
-    console.log("TMP: " + interim_transcript);
-
-    if(final_transcript == "youtube") {
-
-    }
-    // final_span.innerHTML = linebreak(final_transcript);
-    // interim_span.innerHTML = linebreak(interim_transcript);
-    // if (final_transcript || interim_transcript) {
-    //   showButtons('inline-block');
-    // }
-  };
-
-  recognition.start();
-}
+	$('.add').bind('click', function(e) {
+		console.log("ADD");
+	    addRow("", "");
+	});
+});
