076aa6f03570a205a95b992e3c8f9472c776e815
Kevin Casey <kacasey@berkeley.edu>
2015-02-21 13:12:27 -0800

    background voice recognition

--- background.js
+++ background.js
@@ -1,4 +1,35 @@
 // React when a browser action's icon is clicked.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("test");
+
+$(document).ready(function() {
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.onstart = function() {
+    console.log("listen");
+  };
+
+  recognition.onerror = function(event) {
+    console.log("ERROR: " + event.error);
+  };
+
+  recognition.onresult = function(event) {
+    var interim_transcript = '';
+    var final_transcript = '';
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    console.log("FINAL: " + final_transcript);
+    console.log("TMP: " + interim_transcript);
+    // final_span.innerHTML = linebreak(final_transcript);
+    // interim_span.innerHTML = linebreak(interim_transcript);
+    // if (final_transcript || interim_transcript) {
+    //   showButtons('inline-block');
+    // }
+  };
+
+  recognition.start();
 });
--- content.js
+++ content.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-	console.log("HI");
-})
-
-/*var recognition = new webkitSpeechRecognition();
-recognition.onresult = function(event) { 
-  console.log(event) 
-}
-recognition.start();*/
+  console.log("HI");
+  console.log(document.domain);
+});
